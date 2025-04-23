0647dce908782c21fa408c8418d41564ac5ff354
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2025-04-23 11:16:25 +0200

    infer source and sink formats

--- mldataforge/pipelining.py
+++ mldataforge/pipelining.py
@@ -29,20 +29,49 @@ def run_step(defaults, step, named_iterators):
 def load_sources(defaults, sources, named_iterators):
     iterators = []
     for source in sources:
-        fmt = source["fmt"]
+        fmt = None
+        if isinstance(source, dict):
+            if "fmt" in source:
+                fmt = source["fmt"]
+                if "path" in source:
+                    path = get_path(defaults, source["path"])
+                if "name" in source:
+                    name = source["name"]
+            elif "name" in source:
+                fmt = "named"
+                name = source["name"]
+            elif "path" in source:
+                path = get_path(defaults, source["path"])
+                if path.is_dir():
+                    fmt = "mds"
+                elif path.is_file() and path.suffix.lower() == ".parquet":
+                    fmt = "parquet"
+                elif path.is_file() and ".jsonl" in (suffix.lower for suffix in path.suffixes):
+                    fmt = "jsonl"
+        else:
+            path = get_path(defaults, source)
+            if path.is_dir():
+                fmt = "mds"
+            elif path.is_file() and path.suffix.lower() == ".parquet":
+                fmt = "parquet"
+            elif path.is_file() and ".jsonl" in (suffix.lower() for suffix in path.suffixes):
+                fmt = "jsonl"
+            else:
+                fmt = "named"
+                name = source
+            source = {}
         if fmt == "named":
-            name = source["name"]
             if name in named_iterators:
                 iterators.append(named_iterators[name])
                 del named_iterators[name]
             else:
                 raise click.BadArgumentUsage(f"Named iterator '{name}' not found")
         elif fmt == "jsonl":
-            path = get_path(defaults, source["path"])
+            path = str(path)
             ds = load_jsonl_files([path])
             iterators.append(ds)
         elif fmt == "mds":
-            path = get_path(defaults, source["path"])
+            path = str(path)
             ds = load_mds_directories(
                 [path],
                 split=source.get("split", '.'),
@@ -51,7 +80,7 @@ def load_sources(defaults, sources, named_iterators):
             )
             iterators.append(ds)
         elif fmt == "parquet":
-            path = get_path(defaults, source["path"])
+            path = str(path)
             iterators.append(load_parquet_files([path]))
         else:
             raise click.BadArgumentUsage(f"Unknown source format '{fmt}'")
@@ -65,12 +94,40 @@ def save_sink(defaults, sink, ds, trafo, named_iterators):
         for item in ds:
             pass
         return
-    fmt = sink["fmt"]
+    if isinstance(sink, dict):
+        if "fmt" in sink:
+            fmt = sink["fmt"]
+            if "path" in sink:
+                path = get_path(defaults, sink["path"])
+            if "name" in sink:
+                name = sink["name"]
+        elif "name" in sink:
+            fmt = "named"
+            name = sink["name"]
+        elif "path" in sink:
+            path = get_path(defaults, sink["path"])
+            if path.suffix.lower() == ".mds":
+                fmt = "mds"
+            elif path.suffix.lower() == ".parquet":
+                fmt = "parquet"
+            elif ".jsonl" in (suffix.lower() for suffix in path.suffixes):
+                fmt = "jsonl"
+    else:
+        path = get_path(defaults, sink)
+        if path.suffix.lower() == ".mds":
+            fmt = "mds"
+        elif path.suffix.lower() == ".parquet":
+            fmt = "parquet"
+        elif ".jsonl" in (suffix.lower() for suffix in path.suffixes):
+            fmt = "jsonl"
+        else:
+            fmt = "named"
+            name = sink
+        sink = {}
     if fmt == "named":
-        name = sink["name"]
         named_iterators[name] = ds
     elif fmt == "jsonl":
-        path = get_path(defaults, sink["path"])
+        path = str(path)
         save_jsonl(
             ds,
             path,
@@ -82,7 +139,7 @@ def save_sink(defaults, sink, ds, trafo, named_iterators):
             trafo=trafo,
         )
     elif fmt == "mds":
-        path = get_path(defaults, sink["path"])
+        path = str(path)
         save_mds(
             ds,
             path,
@@ -97,7 +154,7 @@ def save_sink(defaults, sink, ds, trafo, named_iterators):
             trafo=trafo,
         )
     elif fmt == "parquet":
-        path = get_path(defaults, sink["path"])
+        path = str(path)
         save_parquet(
             ds,
             path,
@@ -113,4 +170,4 @@ def save_sink(defaults, sink, ds, trafo, named_iterators):
         raise click.BadArgumentUsage(f"Unknown sink format '{fmt}'")
 
 def get_path(defaults, path):
-    return str(Path(defaults["working_dir"]) / path)
+    return Path(defaults["working_dir"]) / path
