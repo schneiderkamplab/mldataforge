8bf82587b774167b8dfeed891588c62971265944
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2025-04-23 16:37:06 +0200

    globs

--- mldataforge/pipelining.py
+++ mldataforge/pipelining.py
@@ -26,52 +26,64 @@ def run_step(defaults, step, named_iterators):
     trafo = step.get("transformations", None)
     save_sink(defaults, step.get("sink", None), ds, trafo, named_iterators)
 
+def _infer_format(source, item, exists):
+    if (exists and item.is_dir()) or item.suffix.lower() == ".mds":
+        return "mds"
+    if (not exists or item.is_file()) and item.suffix.lower() == ".parquet":
+        return "parquet"
+    if (not exists or item.is_file()) and ".jsonl" in (suffix.lower() for suffix in item.suffixes):
+        return "jsonl"
+    if exists:
+        raise click.BadArgumentUsage(f"Cannot resolve item '{item}' from source '{source}'")
+    return "named"
+
+def _resolve_sources(defaults, sources):
+    resolved = []
+    working_dir = Path(defaults["working_dir"])
+    for source in sources:
+        if isinstance(source, dict):
+            if "name" in source:
+                source["fmt"] = "named"
+                resolved.append(source)
+                continue
+            if "path" in source:
+                path = source["path"]
+                found = list(working_dir.glob(path))
+                if not found:
+                    raise click.BadArgumentUsage(f"Cannot find any matches for source '{source}'")
+                for item in found:
+                    res = source.copy()
+                    if not "fmt" in source:
+                        res["fmt"] = _infer_format(source, item, exists=True)
+                    res["path"] = str(item)
+                    resolved.append(res)
+                continue
+            else:
+                raise click.BadArgumentUsage(f"Cannot resolve source '{source}'")
+        if isinstance(source, str):
+            found = list(working_dir.glob(source))
+            if not found:
+                resolved.append({"fmt": "named", "name": source})
+                continue
+            for item in found:
+                resolved.append({"fmt": _infer_format(source, item, exists=True), "path": str(item)})
+    return resolved
+
 def load_sources(defaults, sources, named_iterators):
     iterators = []
-    for source in sources:
-        fmt = None
-        if isinstance(source, dict):
-            if "fmt" in source:
-                fmt = source["fmt"]
-                if "path" in source:
-                    path = get_path(defaults, source["path"])
-                if "name" in source:
-                    name = source["name"]
-            elif "name" in source:
-                fmt = "named"
-                name = source["name"]
-            elif "path" in source:
-                path = get_path(defaults, source["path"])
-                if path.is_dir():
-                    fmt = "mds"
-                elif path.is_file() and path.suffix.lower() == ".parquet":
-                    fmt = "parquet"
-                elif path.is_file() and ".jsonl" in (suffix.lower for suffix in path.suffixes):
-                    fmt = "jsonl"
-        else:
-            path = get_path(defaults, source)
-            if path.is_dir():
-                fmt = "mds"
-            elif path.is_file() and path.suffix.lower() == ".parquet":
-                fmt = "parquet"
-            elif path.is_file() and ".jsonl" in (suffix.lower() for suffix in path.suffixes):
-                fmt = "jsonl"
-            else:
-                fmt = "named"
-                name = source
-            source = {}
+    for source in _resolve_sources(defaults, sources):
+        fmt = source["fmt"]
         if fmt == "named":
+            name = source["name"]
             if name in named_iterators:
                 iterators.append(named_iterators[name])
                 del named_iterators[name]
-            else:
-                raise click.BadArgumentUsage(f"Named iterator '{name}' not found")
-        elif fmt == "jsonl":
-            path = str(path)
-            ds = load_jsonl_files([path])
-            iterators.append(ds)
+                continue
+            raise click.BadArgumentUsage(f"Named iterator '{name}' not found")
+        path = str(source["path"])
+        if fmt == "jsonl":
+            iterators.append(load_jsonl_files([path]))
         elif fmt == "mds":
-            path = str(path)
             ds = load_mds_directories(
                 [path],
                 split=source.get("split", '.'),
@@ -80,14 +92,29 @@ def load_sources(defaults, sources, named_iterators):
             )
             iterators.append(ds)
         elif fmt == "parquet":
-            path = str(path)
             iterators.append(load_parquet_files([path]))
         else:
             raise click.BadArgumentUsage(f"Unknown source format '{fmt}'")
     if all(hasattr(it, "__len__") for it in iterators):
         return ConcatDataset(iterators)
     return itertools.chain(*iterators)
-    
+
+def _resolve_sink(defaults, sink):
+    if isinstance(sink, dict):
+        if "name" in sink:
+            sink["fmt"] = "named"
+            return sink
+        if "path" in sink:
+            if not "fmt" in sink:
+                sink["fmt"] = _infer_format(sink, sink["path"], exists=False)
+            return sink
+        raise click.BadArgumentUsage(f"Cannot resolve sink '{sink}'")
+    if isinstance(sink, str):
+        fmt = _infer_format(sink, sink, exists=False)
+        if fmt == "named":
+            return {"fmt": fmt, "name": sink}
+        return {"fmt": fmt, "path": str(Path(defaults["working_dir"]) / sink)}
+    raise click.BadArgumentUsage(f"Cannot resolve sink '{sink}'")
 
 def save_sink(defaults, sink, ds, trafo, named_iterators):
     if sink is None:
